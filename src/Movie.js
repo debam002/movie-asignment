9094bc9ae8ad25faa5546c9eecae7672a206037e
debam002 <debam0022@gmail.com>
2021-04-16 12:11:51 +0530

    intial commit

---
+++ src/Movie.js
@@ -0,0 +1,38 @@
+import React,{useState} from 'react';
+import {orderBy} from "lodash";
+import data from './movies.json'
+
+
+function Movie() {
+    const [movieList]=useState(data)
+    const {components}=movieList;
+    return (
+        <div>
+            {
+               components?.map((it, i) => {
+                    return (
+                        <div style={{ display: "flex" }}>
+                            {
+                                orderBy(it.items,'releaseDate').map((list,j) =>
+                                
+                                    <div >
+                                        <img style={{ padding: "10px" }} src={list.imageUrl} height="400px" width="250px" alt="" />
+                                        
+                                        <h2>{j+1}</h2>
+                    
+                                         <h3>{list.title}</h3>
+                                         <h3>{list.releaseDate}</h3>
+                                        <p>{list.synopsis}</p>
+                                    </div>
+                                )
+                            }
+                        </div>
+
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Movie
